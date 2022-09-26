acd27fb18f6464761d7621f860dfa6f0bfd89fc2
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 19:58:23 +0300

    Add files via upload

---
+++ Exercise Arrays/02. Common Elements/02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str1 = Console.ReadLine()
+                                   .Split()
+                                   .ToArray();
+
+            string[] str2 = Console.ReadLine()
+                                   .Split()
+                                   .ToArray();
+
+            foreach(string s in str2)
+                foreach(string s1 in str1)
+                    if(s==s1)
+                        Console.Write($"{s} ");
+        }
+    }
+}
