b3ad34db416f5cdcb1ab4f04a3363344cc595745
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 16:17:33 +0300

    Add files via upload

---
+++ Lab Arrays/8. Condense Array to Number/8. Condense Array to Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            while(true)
+            {
+                if (nums.Length == 1)
+                    break;
+
+                for (int i = 0; i <= nums.Length - 2; i++)
+                    nums[i] = nums[i] + nums[i + 1];
+
+                nums = nums.Take(nums.Count() - 1).ToArray();
+            }
+
+            for (int i = 0; i <= nums.Length - 1; i++)
+                Console.Write($"{nums[i]} ");
+        }
+    }
+}
