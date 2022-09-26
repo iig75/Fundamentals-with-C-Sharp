9eeee4a2a70f14e2f770383f2e7a053aaa12536a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 14:09:08 +0300

    Add files via upload

---
+++ Lab Arrays/3. Rounding Numbers/3. Rounding Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            double[] numbers = Array.ConvertAll(str.Split(), Double.Parse);
+
+            foreach (double v in numbers)
+                Console.WriteLine($"{v} => {(int)Math.Round(v, MidpointRounding.AwayFromZero)}");
+        }
+    }
+}
---
+++ Lab Arrays/4. Reverse Array of Strings/4. Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string[] items = str.Split();
+            Array.Reverse(items);
+
+            foreach (string s in items)
+                Console.Write($"{s} ");
+        }
+    }
+}
