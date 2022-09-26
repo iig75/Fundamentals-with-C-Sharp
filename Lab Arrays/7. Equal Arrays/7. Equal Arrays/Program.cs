677351b841aa5bd3c77e732999d4727ea27562dc
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 15:18:38 +0300

    Add files via upload

---
+++ Lab Arrays/7. Equal Arrays/7. Equal Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int[] nums2 = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int sum = 0;
+            int err = -1;
+
+            for (int i = 0; i <= nums1.Length - 1; i++)
+            {
+                if (nums1[i] == nums2[i])
+                    sum += nums1[i];
+                else
+                {
+                    err = i;
+                    break;
+                }                    
+            }
+
+            if(err<0)
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            else
+                Console.WriteLine($"Arrays are not identical. Found difference at {err} index");
+        }
+    }
+}
