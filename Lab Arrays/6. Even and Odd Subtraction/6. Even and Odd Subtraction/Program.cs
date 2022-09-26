bf1fb157449ef216854726bfdb3769c1c7fec3f4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 15:03:08 +0300

    Add files via upload

---
+++ Lab Arrays/6. Even and Odd Subtraction/6. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            for(int i=0; i<=numbers.Length-1; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    sumEven += numbers[i];
+                else
+                    sumOdd += numbers[i];
+            }
+
+            Console.WriteLine($"{sumEven-sumOdd}");
+        }
+    }
+}
