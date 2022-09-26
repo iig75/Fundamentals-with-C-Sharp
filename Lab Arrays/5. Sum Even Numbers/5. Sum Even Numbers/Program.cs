01924510ea7a9b5115bf861124992bc78990c704
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 14:35:32 +0300

    Add files via upload

---
+++ Lab Arrays/5. Sum Even Numbers/5. Sum Even Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int sum = 0;
+
+            for(int i=0; i<=numbers.Length-1; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
