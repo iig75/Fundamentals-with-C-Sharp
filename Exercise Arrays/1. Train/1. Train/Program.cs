63931de9fdadf41fc5d7b72c43751449b4629433
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-26 19:36:13 +0300

    Add files via upload

---
+++ Exercise Arrays/1. Train/1. Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[n];
+
+            for(int i=0; i<=n-1; i++)
+                wagons[i] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n - 1; i++)
+                Console.Write($"{wagons[i]} ");
+
+            Console.WriteLine();
+            
+            Console.WriteLine(wagons.Sum());
+        }
+    }
+}
