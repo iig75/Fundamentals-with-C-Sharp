5f2b86b7c317e89e59a8dfb8f5941a44eaa4033d
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-27 09:22:56 +0300

    Add files via upload

---
+++ 3. Zig-Zag Arrays/3. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+            int[] nums1 = new int[n];
+            int[] nums2 = new int[n];
+
+            int j = 0;
+
+            foreach(int z in nums)
+            {                
+                nums = Console.ReadLine()
+                              .Split()
+                              .Select(int.Parse)
+                              .ToArray();
+
+                if (j % 2 != 0)
+                {
+                    nums1[j] = nums[z+1];
+                    nums2[j] = nums[z];
+                }
+                else
+                {
+                    nums1[j] = nums[z];
+                    nums2[j] = nums[z+1];
+                }
+
+                j++;
+            }
+            
+            foreach(int x in nums1)
+               Console.Write($"{x} ");
+
+            Console.WriteLine();
+
+            foreach (int x in nums2)
+                Console.Write($"{x} ");
+        }
+    }
+}
