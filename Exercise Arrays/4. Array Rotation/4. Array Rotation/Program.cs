359cfd9972790227b6c3863cdcc27e99ea9ac02e
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-28 09:44:44 +0300

    Add files via upload

---
+++ Exercise Arrays/4. Array Rotation/4. Array Rotation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oldArray = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] newArray = new int[oldArray.Length];
+
+            int[] x = new int[n];
+
+            if (n > oldArray.Length)
+                n = n - oldArray.Length;
+
+            for (int i = 0; i <= n - 1; i++)
+                x[i] = oldArray[i];
+
+            Array.Copy(oldArray, n, newArray, 0, oldArray.Length - n);
+
+            int j = 0;
+
+            for (int i = oldArray.Length - n; i <= oldArray.Length - 1; i++)
+            {
+                newArray[i] = x[j];
+                j++;
+            }
+
+            foreach (int f in newArray)
+                Console.Write($"{f} ");
+        }
+    }
+}
