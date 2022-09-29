80bd84b5f82e0ff3e3f8d3ec1aa3ca0652ae54f4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-29 18:02:04 +0300

    Add files via upload

---
+++ Exercise Arrays/6. Equal Sums/6. Equal Sums/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Equal_Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+            int sum1 = 0;
+            int sum2 = 0;
+            int endResult = -99;
+
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                sum1 = 0;
+                sum2 = 0;
+
+                for (int k1 = 0; k1 < i; k1++)
+                {
+                    if (i == 0)
+                        sum1 = 0;
+                    else
+                        sum1 += nums[k1];
+                }
+
+                for (int k2 = i + 1; k2 <= nums.Length - 1; k2++)
+                {
+                    if (i == nums.Length)
+                        sum2 = 0;
+                    else
+                        sum2 += nums[k2];
+                }
+
+                if (sum1 == sum2)
+                {
+                    endResult = i;
+                    break;
+                }
+
+            }
+
+            if (endResult != -99)
+                Console.WriteLine($"{endResult}");
+
+            if (endResult == -99)
+                Console.WriteLine($"no");
+            
+        }
+    }
+}
