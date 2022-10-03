83c945f652e1839c2ef7a585737562ff76731ecb
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-03 16:37:12 +0300

    Add files via upload

---
+++ Exercise Arrays/8. Magic Sum/8. Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<=nums.Length-1; i++)
+                for (int j = i+1; j <= nums.Length-1; j++)
+                    if((nums[i]+nums[j])==n)
+                        Console.WriteLine($"{nums[i]} {nums[j]}");
+        }
+    }
+}
