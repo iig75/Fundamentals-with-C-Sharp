9b2f7013b369d6743801ba1f542fc37c06d9a516
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-05 23:13:38 +0300

    Add files via upload

---
+++ Lab Methods/3. Calculations/3. Calculations/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _3._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            int k = int.Parse(Console.ReadLine());
+
+            int l = int.Parse(Console.ReadLine());
+
+            switch (str)
+            {
+                case "add":
+                    add(k, l);
+                    break;
+
+                case "multiply":
+                    multiply(k, l);
+                    break;
+
+                case "subtract":
+                    subtract(k, l);
+                    break;
+
+                case "divide":
+                    divide(k, l);
+                    break;
+            }
+
+        }
+
+        private static void add(int k, int l)
+        {
+            int result = k + l;
+            Console.WriteLine(result);
+        }
+
+        private static void multiply(int k, int l)
+        {
+            int result = k * l;
+            Console.WriteLine(result);
+        }
+
+        private static void subtract(int k, int l)
+        {
+            int result = k - l;
+            Console.WriteLine(result);
+        }
+
+        private static void divide(int k, int l)
+        {
+            int result = k / l;
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+}
