7175ddc0f19c7e18f95b55b8d7ca1211541cd778
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-05 22:51:39 +0300

    Add files via upload

---
+++ Lab Methods/1. Sign of Integer Numbers/1. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(checkNumber(n));
+
+        }
+
+        private static string checkNumber(int n)
+        {
+            string str="";
+
+            if (n > 0)
+            {
+                str = $"The number {n} is positive.";
+            }
+
+            if (n < 0)
+            {
+                str = $"The number {n} is negative.";
+            }
+
+            if (n == 0)
+            {
+                str = $"The number {n} is zero.";
+            }
+
+            return str;
+        }
+    }
+}
