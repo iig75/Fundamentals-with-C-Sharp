52d581a291b9225a27f9bb8d0316819be8242d0d
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-06 17:32:53 +0300

    Add files via upload

---
+++ Lab Methods/7. Repeat String/7. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(strMethod(str, n));
+        }
+
+        private static string strMethod(string str, int n)
+        {
+            string newStr = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                newStr = string.Concat(newStr, str);
+            }
+
+            return newStr;
+        }
+    }
+}
