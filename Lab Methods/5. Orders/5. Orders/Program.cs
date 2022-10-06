1a5c9d0a117880164419fbd97b1036e8713b9782
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-06 16:01:51 +0300

    Add files via upload

---
+++ Lab Methods/5. Orders/5. Orders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _5._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            int q = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{calc(product, q):f2}");
+        }
+
+        private static double calc(string product, int q)
+        {
+            double sum = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    sum = 1.5 * q;
+                    break;
+
+                case "water":
+                    sum = 1.00 * q;
+                    break;
+
+                case "coke":
+                    sum = 1.40 * q;
+                    break;
+
+                case "snacks":
+                    sum = 2.00 * q;
+                    break;
+            }
+
+            return sum;
+        }
+    }
+}
