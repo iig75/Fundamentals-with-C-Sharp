a628082a4c7d1488b2efcc5de9e91f028bc137c0
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-06 22:09:34 +0300

    Add files via upload

---
+++ Lab Methods/8. Math Power/8. Math Power/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace _8._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(mathPower(n1, n2));
+        }
+
+        private static double mathPower(double n1, double n2)
+        {
+            double res = 1;
+
+            res = Math.Pow(n1, n2);
+
+            return res;
+        }
+    }
+}
