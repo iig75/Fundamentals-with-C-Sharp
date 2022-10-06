a1eef1d01177507d28896586e5376275eb810706
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-06 15:41:37 +0300

    Add files via upload

---
+++ Lab Methods/4. Printing Triangle/4. Printing Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                triangle(i);
+            }
+
+            for (int i = n-1; i >= 1; i--)
+            {
+                triangle(i);
+            }
+        }
+
+        private static void triangle(int b)
+        {
+            for (int i = 1; i <= b; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
