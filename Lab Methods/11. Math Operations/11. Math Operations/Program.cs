21abd717092944be88f85e4a8e8a23177dd038cd
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-07 23:58:51 +0300

    Add files via upload

---
+++ Lab Methods/11. Math Operations/11. Math Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _11._Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{Math(n1, str, n2)}");
+        }
+
+
+        static double Math(int x, string a, int y)
+        {
+            double res = 0;
+
+            switch (a)
+            {
+                case "*":
+                    res = x * y;
+                    break;
+
+                case "/":
+                    res = x / y;
+                    break;
+
+                case "+":
+                    res = x + y;
+                    break;
+
+                case "-":
+                    res = x - y;
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
