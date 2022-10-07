5b927abe96d14cec48f8387fa3a8043b25f7f750
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-07 14:05:58 +0300

    Add files via upload

---
+++ Exercise Methods/6. Middle Characters/6. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            strMethod(str);
+        }
+
+        private static void strMethod(string str)
+        {
+            if ((str.Length) % 2 == 0)
+            {
+                Console.Write($"{str[str.Length / 2-1]}{str[str.Length / 2]}");
+            }
+            else
+            {
+                Console.Write($"{str[str.Length / 2]}");
+            }
+        }
+    }
+}
