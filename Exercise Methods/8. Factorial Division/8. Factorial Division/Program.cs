5634c520da123c9660961fdaef5f68b92bd902d0
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-07 15:07:27 +0300

    Add files via upload

---
+++ Exercise Methods/8. Factorial Division/8. Factorial Division/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _8._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            double res1 = factorial(n1);
+            double res2 = factorial(n2);
+
+            Console.WriteLine($"{(res1 / res2):f2}");
+        }
+
+        private static double factorial(int n1)
+        {
+            double res = 1;
+
+            for (int i = n1 - 1; i >= 1; i--)
+            {
+                res += res * i;
+            }
+
+            return res;
+        }
+    }
+}
+
+
+
+
+
+
+
+
