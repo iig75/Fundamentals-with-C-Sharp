f1fe1d1bf5b2ceb82b9bda6aab9c9171959493f8
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-07 14:29:44 +0300

    Add files via upload

---
+++ Exercise Methods/7. NxN Matrix/7. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            piramida(n);
+        }
+
+        private static void piramida(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
