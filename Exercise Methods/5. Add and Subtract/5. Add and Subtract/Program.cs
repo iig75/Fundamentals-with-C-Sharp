4d015192e0f4d49763efa3892b6897f7d085d53b
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-07 12:14:30 +0300

    Add files via upload

---
+++ Exercise Methods/5. Add and Subtract/5. Add and Subtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            sumSub(n1, n2, n3);
+
+        }
+
+        private static void sumSub(int n1, int n2, int n3)
+        {
+            int sum = n1 + n2;
+            int sub = sum - n3;
+
+            Console.WriteLine($"{sub}");
+        }
+    }
+}
