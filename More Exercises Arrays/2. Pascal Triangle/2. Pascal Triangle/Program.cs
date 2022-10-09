2693dc0016d623519c498cfaef48a0f8a82cfff9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-09 14:28:28 +0300

    Add files via upload

---
+++ More Exercises Arrays/2. Pascal Triangle/2. Pascal Triangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for(int j=0; j<=i; j++)
+                {
+
+                    if ((i==j) || (j == 0) || (j == arr1.Length-1))
+                    {
+                        arr2[j] = 1;
+                        arr1[j] = 1;
+                    }
+                    else
+                    {
+                        arr2[j] = arr1[j-1] + arr1[j];
+                    }
+
+                    Console.Write($"{arr2[j]} ");
+                }
+
+                for (int h = 0; h < arr1.Length; h++)
+                {
+                    arr1[h] = arr2[h];
+                    arr2[h] = 0;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
