9367576f33108cee0484a1c9f99ae484b9cd7290
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-10 16:40:17 +0300

    Add files via upload

---
+++ More Exercises Arrays/4. Fold and Sum/4. Fold and Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int quota = nums.Length / 4;
+
+            int[] nums1 = new int[nums.Length / 2];
+            int[] nums2 = new int[nums.Length / 2];
+
+            for(int i=0; i<quota; i++)
+            {
+                nums1[i] = nums[quota-1-i];
+            }
+
+            for (int i = 0; i < quota; i++)
+            {
+                nums1[quota+i] = nums[nums.Length - 1 - i];
+            }
+
+            for (int i = quota; i < nums.Length - quota; i++)
+            {
+                nums2[i - quota] = nums[i];
+            }
+
+            for(int i = 0; i<nums2.Length; i++)
+            {
+                Console.Write($"{nums1[i] + nums2[i]} ");
+            }
+        }
+    }
+}
