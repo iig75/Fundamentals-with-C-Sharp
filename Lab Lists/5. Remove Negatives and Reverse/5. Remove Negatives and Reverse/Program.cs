fdc5b16c07a4c307df91dd7fff4a87d4d9213024
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-11 21:08:52 +0300

    Add files via upload

---
+++ Lab Lists/5. Remove Negatives and Reverse/5. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+            int flag = 0;
+
+            while(true)
+            {
+                flag = 0;
+
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    if (nums[i] < 0)
+                    {
+                        nums.RemoveAt(i);
+                        flag = 1;
+                    }
+                }
+
+                if (flag == 0)
+                    break;
+            }
+
+            nums.Reverse();
+
+            if (nums.Count == 0)
+                Console.WriteLine("empty");
+            else
+                Console.Write(String.Join(" ", nums));
+        }
+    }
+}
