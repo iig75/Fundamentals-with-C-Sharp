3c703cf9cb8e35f86f0b577aaac8de9d6a68e5fc
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-11 20:55:02 +0300

    Add files via upload

---
+++ Lab Lists/4. List of Products/4. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                products.Insert(i, Console.ReadLine());
+            }
+
+            products.Sort();
+
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine($"{i+1}.{products[i]}");
+            }            
+        }
+    }
+}
