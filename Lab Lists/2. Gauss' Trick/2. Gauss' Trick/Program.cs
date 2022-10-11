87ff11a8d9e488133c154c261f0c830b83786f38
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-11 14:10:57 +0300

    Add files via upload

---
+++ Lab Lists/2. Gauss' Trick/2. Gauss' Trick/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+            for (int i = 0; i < nums.Count / 2; i++)
+            {
+
+
+                Console.Write($"{nums[i]+nums[nums.Count-1-i]} ");
+
+            }
+
+            if((nums.Count) % 2 != 0)
+            {
+                Console.Write($"{nums[nums.Count / 2]} ");
+            }
+        }
+    }
+}
