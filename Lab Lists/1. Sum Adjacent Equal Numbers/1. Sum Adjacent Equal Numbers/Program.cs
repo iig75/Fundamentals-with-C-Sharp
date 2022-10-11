2bd5b12fc302a5782ace777fff057419b239adfd
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-11 09:51:24 +0300

    Add files via upload

---
+++ Lab Lists/1. Sum Adjacent Equal Numbers/1. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Sum_Adjacent_Equal_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine()
+                                    .Split(" ")
+                                    .Select(double.Parse)
+                                    .ToList();
+
+            int flag = 0;
+
+            while (true)
+            {
+                flag = 0;
+
+                for (int i = 0; i < nums.Count - 1; i++)
+                {
+                    if (nums[i] == nums[i + 1])
+                    {
+                        nums[i] += nums[i + 1];
+                        nums.RemoveAt(i + 1);
+                        flag = 1;
+                    }
+                }
+
+                if (flag == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
