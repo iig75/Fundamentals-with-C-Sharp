fd97b3b2cb663ac4f910e72f35178f0fa605b6d6
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-11 23:21:04 +0300

    Add files via upload

---
+++ Lab Lists/6. List Manipulation Basics/6. List Manipulation Basics/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end")
+                    break;
+
+                string[] str = new string[3];
+
+                str = command.Split();
+
+                if (str[0] == "Add")
+                    add(nums, int.Parse(str[1]));
+
+
+                if (str[0] == "Remove")
+                    remove(nums, int.Parse(str[1]));
+
+                if (str[0] == "RemoveAt")
+                    removeat(nums, int.Parse(str[1]));
+
+                if (str[0] == "Insert")
+                    insert(nums, int.Parse(str[1]), int.Parse(str[2]));
+
+            }
+
+            Console.Write(String.Join(" ", nums));
+        }
+
+
+
+        static void add(List<int> z, int x)
+        {
+            z.Add(x);
+        }
+
+
+
+        static void remove(List<int> z, int x)
+        {
+            z.Remove(x);
+        }
+
+
+
+        static void removeat(List<int> z, int x)
+        {
+            z.RemoveAt(x);
+        }
+
+
+
+        static void insert(List<int> z, int x, int y)
+        {
+            z.Insert(y, x);
+        }
+
+
+
+    }
+}
