631157d82d2d1a35c4cb552e9a6c4acdb627dee1
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-19 15:08:10 +0300

    Add files via upload

---
+++ Exercise Lists/6. Cards Game/6. Cards Game/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Cards_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> cards1 = Console.ReadLine()
+                                      .Split()
+                                      .Select(int.Parse)
+                                      .ToList();
+
+            List<int> cards2 = Console.ReadLine()
+                                      .Split()
+                                      .Select(int.Parse)
+                                      .ToList();
+
+            int i = 0;
+
+            while(true)
+            {
+                if(cards1[i]>cards2[i])
+                {
+                    cards1.Add(cards1[i]);
+                    cards1.Add(cards2[i]);
+                }
+
+                if (cards2[i] > cards1[i])
+                {
+                    cards2.Add(cards2[i]);
+                    cards2.Add(cards1[i]);
+                }
+
+                cards1[i] = 0;
+                cards2[i] = 0;
+
+                if ((cards1.Sum() == 0) || (cards2.Sum() == 0))
+                {
+                    break;
+                }
+
+                i++;
+            }
+
+            if (cards1.Sum() == 0)
+                Console.WriteLine($"Second player wins! Sum: {cards2.Sum()}");
+            else
+                Console.WriteLine($"First player wins! Sum: {cards1.Sum()}");
+        }
+    }
+}
