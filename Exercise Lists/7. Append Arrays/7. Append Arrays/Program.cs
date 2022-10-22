20a14abe9bb585793722aa32494c5c057373bcd7
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-22 16:25:41 +0300

    Add files via upload

---
+++ Exercise Lists/7. Append Arrays/7. Append Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> BigList = Console.ReadLine()
+                                          .Split("|")
+                                          .Reverse()
+                                          .ToList();
+
+            List<string> SmallList = new List<string>();
+
+            for (int i = 0; i < BigList.Count; i++)
+            {
+                SmallList = BigList[i].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .ToList();
+
+                for (int j = 0; j < SmallList.Count; j++)
+                {
+                    Console.Write($"{SmallList[j]} ");
+                }
+            }
+        }
+    }
+}
