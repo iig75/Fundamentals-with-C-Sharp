66aea5ddb53fd2792ae308e9883424a9de96292c
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-25 14:00:22 +0300

    Add files via upload

---
+++ More Exercises Lists/2. Car Race/2. Car Race/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+            double LSum = 0;
+            double RSum = 0;
+
+            for (int i = 0; i < nums.Count / 2; i++)
+            {
+                LSum += nums[i];
+
+                if(nums[i]==0)
+                {
+                    LSum -= LSum * 0.2;
+                }
+            }
+
+            for (int i = nums.Count-1; i > nums.Count / 2; i--)
+            {
+                RSum += nums[i];
+
+                if (nums[i] == 0)
+                {
+                    RSum -= RSum * 0.2;
+                }
+            }
+
+            if(LSum<RSum)
+                Console.WriteLine($"The winner is left with total time: {LSum}");
+            else
+                Console.WriteLine($"The winner is right with total time: {RSum}");
+
+        }
+    }
+}
