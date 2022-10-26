e55df6092a5e4ef9a3dc10cf3d2a276cf1aea7b1
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-26 09:23:31 +0300

    Add files via upload

---
+++ More Exercises Lists/1. Messaging/1. Messaging/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine()
+                                    .Split()
+                                    .ToList();
+
+            string str = Console.ReadLine();
+            string result = String.Empty;
+
+            int sum = 0;
+
+            foreach(string n in nums)
+            {
+                sum = 0;
+
+                for(int i=0; i<n.Length; i++)
+                {
+                    sum += int.Parse(n[i].ToString());
+                }
+
+                if ((sum < str.Length) && (str.Length>0))
+                {
+                    result += str[sum].ToString();
+                    str = str.Remove(sum, 1);
+                }
+
+                if ((sum > str.Length) && (str.Length > 0))
+                {
+                    sum = sum - (str.Length*(sum/str.Length));
+                    result += str[sum].ToString();
+                    str = str.Remove(sum, 1);
+
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
