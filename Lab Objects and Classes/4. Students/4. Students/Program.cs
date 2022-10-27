3d45b8e6a60d65dc7515d1480e9433b4bf723c3b
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-27 12:21:33 +0300

    Add files via upload

---
+++ Lab Objects and Classes/4. Students/4. Students/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>();
+            
+            while(true)
+            {
+                List<string> input = Console.ReadLine()
+                                            .Split()
+                                            .ToList();
+
+                if (input[0] == "end")
+                    break;
+
+                Student students = new Student(input[0], input[1], int.Parse(input[2]), input[3]);
+
+                student.Add(students);
+            }
+
+            string homeTown = Console.ReadLine();            
+
+            for(int i=0; i<student.Count; i++)
+            {
+                Student currentStudents = student[i];
+
+                if(currentStudents.Town == homeTown)
+                {
+                    Console.WriteLine($"{currentStudents.FirstName} {currentStudents.LastName} is {currentStudents.Age} years old.");
+                }
+            }
+        }
+    }
+
+
+
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string town)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Town = town;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+    }
+
+
+
+}
