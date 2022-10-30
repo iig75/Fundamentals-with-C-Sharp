ef2b300e19be8f4b0d7db5d05abc6f85c27855b9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-10-30 22:59:00 +0200

    Add files via upload

---
+++ Exercise Objects and Classes/4. Students/4. Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for(int i=1; i<=n; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+
+                Student student = new Student(input[0], input[1], double.Parse(input[2]));
+
+                students.Add(student);
+
+                students = students.OrderByDescending(x => x.Grade).ToList();
+            }
+
+            for(int i=0; i<students.Count; i++)
+            {
+                Student currentStudent = students[i];
+
+                Console.WriteLine($"{currentStudent.FirstName} {currentStudent.LastName}: {currentStudent.Grade:f2}");
+            }
+        }
+    }
+
+
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+    }
+}
