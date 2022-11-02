0374e6843fee2bfbeb9a309fbaddd91b5c6780ec
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-02 23:50:05 +0200

    Add files via upload

---
+++ Lab Dictionaries, Lambda and LINQ/1. Count Real Numbers/1. Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (counts.ContainsKey(nums[i]))
+                {
+                    counts[nums[i]]++;
+                }
+                else
+                {
+                    counts.Add(nums[i], 1);
+                }
+            }
+
+            foreach(var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }            
+        }
+    }
+}
---
+++ Lab Dictionaries, Lambda and LINQ/2. Odd Occurrences/2. Odd Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordToLowerCase = word.ToLower();
+
+                if(counts.ContainsKey(wordToLowerCase))
+                {
+                    counts[wordToLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordToLowerCase, 1);
+                }
+            }
+
+            foreach(var val in counts)
+            {
+                if(val.Value % 2 != 0)
+                {
+                    Console.Write(val.Key +" ");
+                }
+            }
+        }
+    }
+}
