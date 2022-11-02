20b73b438d81241bb7dd099fea0f39f9d18a74e3
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-02 23:51:10 +0200

    Add files via upload

---
+++ Lab Dictionaries, Lambda and LINQ/3. Word Synonyms/3. Word Synonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+
+                words[word].Add(synonyms);
+            }
+
+            foreach(var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {String.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
---
+++ Lab Dictionaries, Lambda and LINQ/4. Word Filter/4. Word Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                    .Split()
+                                    .Where(x => x.Length % 2 == 0)
+                                    .ToArray();
+
+            Console.WriteLine(String.Join(Environment.NewLine, words));
+        }
+    }
+}
