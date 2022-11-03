caf75ab79e9e7ce7c836a8faa9962b3cd3c2be86
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-03 10:44:15 +0200

    Add files via upload

---
+++ Exercise Associative Arrays/2. A Miner Task/2. A Miner Task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> res = new Dictionary<string, int>();
+
+            int i = 0;
+            
+            string key = String.Empty;
+
+            while (true)
+            {
+                i++;
+
+                string str = Console.ReadLine();
+
+                if (str == "stop")
+                    break;
+
+                if (i % 2 != 0)
+                {
+                    key = str;
+
+                    if (!res.ContainsKey(str))
+                    {
+                        res.Add(str, 0);                        
+                    }
+                }
+                else
+                {
+                    res[key] += int.Parse(str);
+                }
+            }
+
+            foreach (var val in res)
+            {
+                Console.WriteLine($"{val.Key} -> {val.Value}");
+            }
+        }
+    }
+}
