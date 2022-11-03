291664eb435695f66c3f6d0eb9253c1c2bdc7831
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-03 16:21:37 +0200

    Add files via upload

---
+++ Exercise Associative Arrays/5. Courses/5. Courses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            
+            while (true)
+            {
+                List<string> command = Console.ReadLine().Split(" : ").ToList();
+
+                if (command[0] == "end")
+                    break;
+
+                if(!courses.ContainsKey(command[0]))
+                {
+                    courses.Add(command[0], new List<string>());
+                    courses[command[0]].Add(command[1]);
+                }
+                else
+                {
+                    courses[command[0]].Add(command[1]);
+                }
+            }
+
+
+            foreach(var val in courses)
+            {
+                Console.WriteLine($"{val.Key}: {val.Value.Count}");
+
+                foreach(var name in val.Value)
+                    Console.WriteLine($"-- {name}");
+            }
+        }
+    }
+}
