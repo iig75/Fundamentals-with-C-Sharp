5c0f4adaeb9632c67501efac7e997bc467dac127
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-03 12:06:18 +0200

    Add files via upload

---
+++ Exercise Associative Arrays/3. Orders/3. Orders/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> orders = new Dictionary<string, string>();
+
+            while(true)
+            {
+                List<string> command = Console.ReadLine()
+                                              .Split()
+                                              .ToList();
+
+                if (command[0] == "buy")
+                    break;
+
+                if(!orders.ContainsKey(command[0]))
+                {
+                    orders.Add(command[0], $"{command[1]}:{command[2]}");
+                }
+                else
+                {
+                    List<string> A = orders[command[0]].Split(":").ToList();
+
+                    A[1] = (int.Parse(A[1]) + int.Parse(command[2])).ToString();
+
+                    if (double.Parse(A[1]) > double.Parse(command[1]))
+                        A[0] = command[1];
+
+                    orders[command[0]] = $"{A[0]}:{A[1]}";
+                }
+            }
+
+            foreach (var val in orders)
+            {
+                List<string> B = val.Value.Split(":").ToList();
+
+                Console.WriteLine($"{val.Key} -> {(double.Parse(B[0])*int.Parse(B[1])):f2}");
+            }
+        }
+    }
+}
