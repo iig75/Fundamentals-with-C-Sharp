752f565cdfde51050a62e0f2562f2a5847188544
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-03 09:32:41 +0200

    Add files via upload

---
+++ Exercise Associative Arrays/1. Count Chars in a String/1. Count Chars in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> count = new Dictionary<char, int>();
+
+            string str = Console.ReadLine();
+
+            for(int i=0; i<str.Length; i++)
+            {
+                if (str[i] != (char)32)
+                {
+                    if (count.ContainsKey(str[i]))
+                    {
+                        count[str[i]]++;
+                    }
+                    else
+                    {
+                        count.Add(str[i], 1);
+                    }
+                }
+            }
+
+            foreach(var val in count)
+            {
+                Console.WriteLine($"{val.Key} -> {val.Value}");
+            }
+        }
+    }
+}
