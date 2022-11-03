7dffc72eed1178bc3931cf136b9aa7ab7442d2ee
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-03 22:30:31 +0200

    Add files via upload

---
+++ Exercise Associative Arrays/6. Student Academy/6. Student Academy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string studentsName = Console.ReadLine();
+                double studentsGrade = double.Parse(Console.ReadLine());
+
+                if(!students.ContainsKey(studentsName))
+                {
+                    students.Add(studentsName, new List<double>());
+                    students[studentsName].Add(studentsGrade);
+                }
+                else
+                {
+                    students[studentsName].Add(studentsGrade);
+                }
+            }
+
+            foreach(var val in students)
+            {
+                double sum = 0;
+                int i = 0;
+
+                foreach(var grade in val.Value)
+                {
+                    sum += grade;
+                    i++;
+                }
+                
+                if((sum / i)>=4.5)
+                {
+                    Console.WriteLine($"{val.Key} -> {(sum / i):f2}");
+                }
+            }
+        }
+    }
+}
