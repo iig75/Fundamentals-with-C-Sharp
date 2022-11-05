6bb870bb4f9dcf208b53ef20fa5c1641cb65911a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-05 23:23:31 +0200

    Add files via upload

---
+++ Lab Objects and Classes/2. Big Factorial/2. Big Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
