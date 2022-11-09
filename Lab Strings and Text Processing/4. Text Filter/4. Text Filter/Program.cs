b124fb4ccaa60f23d67679886766de53e1051e5f
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-09 23:08:43 +0200

    Add files via upload

---
+++ Lab Strings and Text Processing/4. Text Filter/4. Text Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> banList = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string str = Console.ReadLine();
+
+            foreach(var val in banList)
+            {
+                while (str.Contains(val))
+                {
+                    str = str.Replace(val, new string('*', val.Length));
+                }
+            }
+
+            Console.WriteLine($"{str}");
+        }
+    }
+}
