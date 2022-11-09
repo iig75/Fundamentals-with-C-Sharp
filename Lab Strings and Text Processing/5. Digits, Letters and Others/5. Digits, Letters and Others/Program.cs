ae60a97727208d5d3cd7956e5f68aa4015811ed3
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-09 23:22:09 +0200

    Add files via upload

---
+++ Lab Strings and Text Processing/5. Digits, Letters and Others/5. Digits, Letters and Others/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _5._Digits__Letters_and_Others
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string digits = String.Empty;
+            string letters = String.Empty;
+            string symbols = String.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (Char.IsDigit(str[i]))
+                {
+                    digits += str[i];
+                }
+                else if (Char.IsLetter(str[i]))
+                {
+                    letters += str[i];
+                }
+                else
+                {
+                    symbols += str[i];
+                }
+            }
+
+            Console.WriteLine($"{digits}");
+            Console.WriteLine($"{letters}");
+            Console.WriteLine($"{symbols}");
+        }
+    }
+}
