ba98827cba8a6d8c4ffc458513ebd1d523d517b7
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-10 17:30:12 +0200

    Add files via upload

---
+++ Exercise Strings and Text Processing/4. Caesar Cipher/4. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string res = String.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                res += ((char)((int)str[i] + 3)).ToString();
+            }
+
+            Console.WriteLine($"{res}");
+        }
+    }
+}
