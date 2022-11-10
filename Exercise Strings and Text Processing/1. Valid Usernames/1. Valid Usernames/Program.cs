97fdc48ce7550706220d154d1a6ae4ad24ab8c20
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-10 16:32:19 +0200

    Add files via upload

---
+++ Exercise Strings and Text Processing/1. Valid Usernames/1. Valid Usernames/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = Console.ReadLine()
+                                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                            .ToList();
+
+            foreach (var val in usernames)
+            {
+                bool err = false;
+
+                for(int i=0; i<val.Length; i++)
+                {
+                    if ((!Char.IsLetter(val[i])) && (!Char.IsDigit(val[i])) && (val[i]!='_') && (val[i]!='-'))
+                    {
+                        err = true;
+                    }
+                }
+
+                if((val.Length < 3) || (val.Length>16))
+                {
+                    err = true;
+                }
+
+                if(!err)
+                {
+                    Console.WriteLine($"{val}");
+                }
+            }
+        }
+    }
+}
---
+++ Exercise Strings and Text Processing/3. Extract File/3. Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string filename = str.Substring(str.LastIndexOf('\\')+1, str.LastIndexOf('.') - 1 - str.LastIndexOf('\\'));
+
+            string ext = str.Substring(str.LastIndexOf('.')+1, str.Length-1 - str.LastIndexOf('.'));
+
+            Console.WriteLine($"File name: {filename}");
+            Console.WriteLine($"File extension: {ext}");
+        }
+    }
+}
