89886bab2bc378536ef6498b8330c0163c05f783
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-12 15:45:51 +0200

    Add files via upload

---
+++ Exercise Strings and Text Processing/6. Replace Repeating Chars/6. Replace Repeating Chars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _6._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = String.Empty;
+
+            str2 += str1[0];
+            int h = 0;
+
+            for (int i = 1; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[h])
+                {
+                    str2 += str1[i];
+                    h++;
+                }
+            }
+
+            Console.WriteLine($"{str2}");
+        }
+    }
+}
