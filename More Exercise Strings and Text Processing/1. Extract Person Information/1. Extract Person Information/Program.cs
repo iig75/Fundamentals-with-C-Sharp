ae826eef747487c19dd2dcd959037d712ffa046a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-12 16:03:54 +0200

    Add files via upload

---
+++ More Exercise Strings and Text Processing/1. Extract Person Information/1. Extract Person Information/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                string str = Console.ReadLine();
+
+                string name = str.Substring(str.IndexOf('@')+1,str.IndexOf('|') - str.IndexOf('@')-1);
+                string age = str.Substring(str.IndexOf('#')+1, str.IndexOf('*') - str.IndexOf('#')-1);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
