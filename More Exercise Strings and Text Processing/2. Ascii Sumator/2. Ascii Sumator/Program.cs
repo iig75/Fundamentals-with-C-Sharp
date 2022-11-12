b123e351a9925e9bc86dcb4466f03e2709a7dd3f
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-12 16:52:03 +0200

    Add files via upload

---
+++ More Exercise Strings and Text Processing/2. Ascii Sumator/2. Ascii Sumator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input1 = char.Parse(Console.ReadLine());
+            char input2 = char.Parse(Console.ReadLine());
+            string randomString = Console.ReadLine();
+
+            int start = Math.Min(input1, input2);
+            int stop = Math.Max(input1, input2);
+
+            int sum = 0;
+
+            for (int i=0; i<randomString.Length; i++)
+            {
+                if(((int)randomString[i] > start) && ((int)randomString[i] < stop))
+                {
+                    sum += (int)randomString[i];
+                }
+            }
+
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
