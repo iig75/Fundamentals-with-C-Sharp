a957221cbdd551ad14e7e332f36d1c98d915d8d9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-12 15:42:28 +0200

    Add files via upload

---
+++ Exercise Strings and Text Processing/7. String Explosion/7. String Explosion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();   // abv>1>1>2>2asdasd
+
+            string res = String.Empty;
+
+            int bomb = 0;
+
+            for(int i=0; i<str.Length; i++)
+            {
+                if(str[i]=='>')
+                {
+                    bomb += int.Parse(str[i+1].ToString());
+                }
+
+                if ((bomb > 0) && (str[i] != '>'))
+                {
+                    str = str.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
