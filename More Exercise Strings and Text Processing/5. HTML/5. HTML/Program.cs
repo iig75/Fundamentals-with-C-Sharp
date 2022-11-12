799b5174c53292b0fd9c84cafe5fb2db398401b4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-12 22:35:34 +0200

    Add files via upload

---
+++ More Exercise Strings and Text Processing/5. HTML/5. HTML/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+
+            string content = Console.ReadLine();
+
+            List<string> comments = new List<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end of comments")
+                    break;
+                else
+                    comments.Add(command);
+            }
+
+            Console.WriteLine($"<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine($"</h1>");
+
+            Console.WriteLine($"<article>");
+            Console.WriteLine($"    {content}");
+            Console.WriteLine($"</article>");
+
+            for(int i=0; i<comments.Count; i++)
+            {
+                Console.WriteLine($"<div>");
+                Console.WriteLine($"    {comments[i]}");
+                Console.WriteLine($"</div>");
+            }
+                
+        }
+    }
+}
