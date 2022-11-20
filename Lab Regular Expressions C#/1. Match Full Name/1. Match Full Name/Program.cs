cb8f1d148522fc97434aa4ba0cda8747c3c3fef8
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-20 23:20:51 +0200

    Add files via upload

---
+++ Lab Regular Expressions C#/1. Match Full Name/1. Match Full Name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex reg = new Regex(pattern);
+
+            if (reg.IsMatch(str))
+            {
+                foreach(Match match in reg.Matches(str))
+                {
+                    Console.Write($"{match.Value} ");
+                }
+            }
+        }
+    }
+}
---
+++ Lab Regular Expressions C#/2. Match Phone Number/2. Match Phone Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string pattern = @"\+\b359( |-)2\1[0-9]{3}\1[0-9]{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            string result = String.Empty;
+
+            if(regex.IsMatch(str))
+            {
+                foreach(Match match in regex.Matches(str))
+                {
+                    result += $"{match.Value}, ";
+                }
+
+                result = result.Remove(result.Length - 2, 2);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
