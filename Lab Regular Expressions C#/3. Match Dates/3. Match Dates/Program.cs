21de57ac43286c38b8de68ee01bed41e96047aab
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-21 15:57:47 +0200

    Add files via upload

---
+++ Lab Regular Expressions C#/3. Match Dates/3. Match Dates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string pattern = @"(?<day>\d{2})(/|.|-)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            Regex regex = new Regex(pattern);
+
+            if(regex.IsMatch(str))
+            {
+                foreach (Match date in regex.Matches(str))
+                {
+                    string day = date.Groups["day"].Value;
+                    string month = date.Groups["month"].Value;
+                    string year = date.Groups["year"].Value;
+
+                    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+                }
+            }
+        }
+    }
+}
