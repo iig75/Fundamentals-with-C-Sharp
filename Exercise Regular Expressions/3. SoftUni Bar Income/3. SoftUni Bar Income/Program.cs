e0a4fc13b3211d5af78ca877a899ecf0b8baeb76
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-22 16:35:16 +0200

    Add files via upload

---
+++ Exercise Regular Expressions/3. SoftUni Bar Income/3. SoftUni Bar Income/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^[^\|\$\%\.]*?\%(?<name>[A-Z][a-z]+)\%[^\|\$\%\.]*?\<(?<product>\w+)\>[^\|\$\%\.]*?\|(?<quantity>\d+)\|[^\|\$\%\.]*?(?<price>\d+(.\d+)?)\$[^\|\$\%\.]*?$";
+
+            string str = String.Empty;
+
+            double totalIncome = 0;
+
+            Regex regex = new Regex(pattern);
+
+            while(true)
+            {
+                str = Console.ReadLine();
+
+                if (str == "end of shift")
+                    break;
+
+                if(regex.IsMatch(str))
+                {
+                    foreach(Match match in regex.Matches(str))
+                    {
+                        Console.WriteLine($"{match.Groups["name"].Value}: {match.Groups["product"].Value} - {(double.Parse(match.Groups["quantity"].Value) * double.Parse(match.Groups["price"].Value)):f2}");
+
+                        totalIncome += (double.Parse(match.Groups["quantity"].Value) * double.Parse(match.Groups["price"].Value));
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
