600e580e22fba221749f71088ef2e34684adf4ca
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-28 23:12:17 +0200

    Add files via upload

---
+++ Exam Preparation/02. Programming Fundamentals Final Exam/Problem 2 - Destination Mapper/Problem 2 - Destination Mapper/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Problem_2___Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\=|\/)([A-Z][A-Za-z]{2,})(\1)";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            string output = String.Empty;
+
+            int len = 0;
+
+            foreach(Match m in matches)
+            {
+                output += m.Value +", ";
+                len += m.Value.Length - 2;
+            }
+
+            if(output.Length>0)
+            {
+                output = output.Remove(output.Length - 2, 1);
+                output = output.Replace("=", "");
+                output = output.Replace("/", "");
+            }
+
+            Console.WriteLine($"Destinations: {output}");
+            Console.WriteLine($"Travel Points: {len}");
+        }
+    }
+}
