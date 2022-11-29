e9117efce7b5467d1d2a7faa49a30aae8d8e501b
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-29 11:52:32 +0200

    Add files via upload

---
+++ Exam Preparation/01. Programming Fundamentals Final Exam Retake/Problem 2 - Ad Astra/Problem 2 - Ad Astra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Problem_2___Ad_Astra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\||\#)(?<food>[A-Za-z ]+)(\1)(?<date>[0-9]{2}(\/)[0-9]{2}(\/)[0-9]{2})(\1)(?<cal>[0-9]+)(\1)";
+
+            string input = Console.ReadLine();
+
+            int calories = 0;
+
+            MatchCollection mathes = Regex.Matches(input, pattern);
+
+            foreach (Match m in mathes)
+            {
+                calories += int.Parse(m.Groups["cal"].Value);
+            }
+
+            Console.WriteLine($"You have food to last you for: {calories / 2000} days!");
+            
+            foreach (Match m in mathes)
+            {
+                Console.WriteLine($"Item: {m.Groups["food"].Value}, Best before: {m.Groups["date"].Value}, Nutrition: {m.Groups["cal"].Value}");
+            }
+        }
+    }
+}
