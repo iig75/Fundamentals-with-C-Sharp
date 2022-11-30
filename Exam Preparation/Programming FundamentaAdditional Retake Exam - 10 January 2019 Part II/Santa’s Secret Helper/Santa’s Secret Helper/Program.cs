78ebfe07b1cc89e6d21953df7f1a983d2346730b
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-11-30 09:22:09 +0200

    Add files via upload

---
+++ Exam Preparation/Programming FundamentaAdditional Retake Exam - 10 January 2019 Part II/Santa’s Secret Helper/Santa’s Secret Helper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string pattern = @"@(?<name>[A-Za-z]+)([^@\-!:>]*!)(?<pos>[G])(!)";
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                string str1 = String.Empty;
+
+                if (str == "end")
+                    break;
+
+                for (int h = 0; h < str.Length; h++)
+                {
+                     str1 += ((char)((int)str[h] - n)).ToString();
+                }
+
+                Regex regex = new Regex(pattern);
+
+                if(regex.IsMatch(str1))
+                {
+                    Console.WriteLine($"{regex.Match(str1).Groups["name"].Value}");
+                }
+            }
+        }
+    }
+}
